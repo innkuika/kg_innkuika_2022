d49169930ca44b465baff3da718fca9def8ae4da
innkuika <innku.ika@gmail.com>
2021-04-26 15:30:00 -0700

    solution code

--- main.js
+++ main.js
@@ -3,3 +3,20 @@
 // For example:
 //     Given an array: [3, 25, 209], print “Three,TwoFive,TwoZeroNine” into stdout.
 //     Given an array: [10, 300, 5], print “OneZero,ThreeZeroZero,Five” into stdout.
+
+const phoneticMap = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+let solution = "";
+
+for (let i = 2; i < process.argv.length; i++) {
+    let number_string = process.argv[i].toString();
+    for (let j = 0; j < number_string.length; j++) {
+        solution += `${phoneticMap[parseInt(number_string[j])]}`
+    }
+
+    if (i === process.argv.length - 1) {
+        break;
+    }
+    solution += `, `
+
+}
+console.log(solution);
