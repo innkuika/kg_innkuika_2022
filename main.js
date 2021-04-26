232929a615f349ffb0b54a11686cab52d87e48e1
innkuika <innku.ika@gmail.com>
2021-04-26 15:40:33 -0700

    documentation

--- main.js
+++ main.js
@@ -7,12 +7,16 @@
 const phoneticMap = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
 let solution = "";
 
+// loop through all input arguments
 for (let i = 2; i < process.argv.length; i++) {
-    let number_string = process.argv[i].toString();
+    let number_string = process.argv[i];
+    // loop through each digit of number_string
     for (let j = 0; j < number_string.length; j++) {
+        // convert each digit and append to solution
         solution += `${phoneticMap[parseInt(number_string[j])]}`
     }
 
+    // if we are at the last argument, don't append comma to solution
     if (i === process.argv.length - 1) {
         break;
     }
